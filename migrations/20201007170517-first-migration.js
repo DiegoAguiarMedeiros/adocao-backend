b3c98383e8e3bf17477d970fa994824f31950220
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2022-10-08 15:30:09 -0300

    add first migration

---
+++ migrations/20201007170517-first-migration.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async up(db, client) {
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  },
+
+  async down(db, client) {
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  },
+};
