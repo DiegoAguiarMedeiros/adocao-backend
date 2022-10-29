50da66e4c310890aac9181f5b7cf18067fa92cd0
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2022-10-29 14:30:48 -0300

    add couldinary para salvar as imagens

--- src/services/implementations/S3ImageService.ts
+++ src/services/implementations/S3ImageService.ts
@@ -2,23 +2,52 @@ import crypto from "crypto";
 import * as fs from 'fs';
 import { IImageService } from "../IImageService";
 
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: 'dqxt4kisi',
+  api_key: '185631269744387',
+  api_secret: 'QR5LFPYfbbbqKKVWFfS4cr_EETg'
+});
+
+
+const uploadPicture = (content: Buffer): Promise<object> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream(
+      {
+        folder: 'profile_pictures',
+        eager: [{ width: 400, height: 400, crop: 'crop', gravity: 'face' }]
+      }, (error, result) => {
+        if (error) {
+          throw new Error('Upload failed')
+        } else {
+          resolve(result)
+        }
+      }
+    ).end(content)
+  })
+}
+
+const uploadProfilePicture = async (img: any): Promise<any> => {
+  const binaryData = img
+  const result = await uploadPicture(binaryData) // Watch below for details
+  return result
+}
+
+// const uploadProfilePicture = async (req, res) => Promise < string > {
+//   const binaryData = req.file.buffer
+//   const result = await uploadPicture(binaryData) // Watch below for details
+//   return result.url
+// }
 export default class S3ImageService implements IImageService {
   async saveImage(imageBuffer: Buffer, fileMimetype: string, petId: string): Promise<string> {
     const imageType = fileMimetype.split("/")[1];
-    const imageName = `${crypto.randomBytes(16).toString("hex")}.${imageType}`;
-    const path = `./uploads/${petId}`;
 
-    fs.mkdirSync(path, { recursive: true })
+    const urlCloudinary = await uploadProfilePicture(imageBuffer);
 
-    const fullPath = `${path}/${imageName}`
+    console.log('urlCloudinary', urlCloudinary)
 
-    fs.writeFile(fullPath, imageBuffer, "binary", (err) => {
-      if (err)  throw new Error('UploadPicturePetUseCase: not saved');;
-    });
-
-    const finalPath = fullPath.substring(1)
-
-    return finalPath;
+    return urlCloudinary.secure_url;
   }
 
   async deleteImage(fileName: string): Promise<any> {
